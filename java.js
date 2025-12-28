9a3e4e5d4ab09fc6851a974128ee840c66b06a38
atahoseinnezhad <atahoseinnezhad@gmail.com>
2025-12-28 15:43:38 +0330

    first

---
+++ java.js
@@ -0,0 +1,391 @@
+  /*const firstNum =20;
+const secondNum = 2;
+const result = firstNum * secondNum;
+ console.log(result);*/
+/*const firstNum = 1200 
+const secondNum = 2
+const result = firstNum-secondNum
+console.log(result); */
+//var nember,Name,LastName,x,x;
+//nember=6;
+//Name ="a t  a  ";
+//console.log(Name);
+ // window.alert(Name);
+ //var number=6;
+// var Name = "ata hasta";
+// var variable="cide yad "+6;
+ //var variable2= 16 - 6  + "codeyad";
+ //var variable3= "codeyad" + 16 + 6;
+// var person ={firstName:"ata",LastName:"hoseini"};
+//document.write(person.firstName+
+// const name1=["salam"];
+// const name2=["salam"];
+// console.log(name1==name2);
+// let string="Salam Khobi Agha Pesar";
+// console.log(string.length);
+// console.log(string.toLowerCase())
+// console.log(string.toUpperCase())
+// console.log(string.indexOf("b"))
+// console.log(string.charAt(9))
+// console.log(string.split(" "));
+// console.log(string.substring(4,8));
+// console.log(string.replace("Agha","khanom"));
+// console.log(string.includes("Pesar"));
+// console.log();
+
+
+// qwe55 = "its enough";
+// let qwe00 = "hi";
+// let qwe02 ="im24";
+// let qwe0= "hello";
+// let qwe2 = "how old are you my brother";
+// let qwe1 = "how you doing bro ";
+// let qwe01 = "im learning code";
+// const newqwe=`${qwe0}?
+// ${qwe00}
+//   ${qwe1}?
+//    ${qwe01}
+//     ${qwe2}?
+//      ${qwe02}
+//       ${qwe55}>< 
+//         `;
+// console.log(newqwe);
+// const names = [ "ali" ,"giii","hajat","port" ];
+// names.push("ata");
+// names.unshift("attttta giii ");
+// console.log(names);
+// let  names = [["Milad", "Ali"],["ali","mmd"] ,["John", "Thomas", "David"]];
+// let name1= names[0];
+// let name2= names[1];
+// let name3= names[2];
+// let newname =[...name1,...name2,...name3];
+// let sortedname = newname.sort();
+// console.log(sortedname);
+// let name  = sortedname[6];
+// let resheh = `hi ${name}`;
+// console.log(resheh);
+// const data = {
+//     id: 1,
+//     name: "Milad",
+//     username: "Bret",
+//     email: "Sincere@april.biz",
+//     address: {
+//         street: "Kulas Light",
+//         suite: "Apt. 556",
+//         city: "Gwenborough",
+//         zipcode: "92998-3874",
+//         geo: {
+//             name:"ata" ,
+//             lat: "-37.3159",
+//             lng: "81.1496"
+//         }
+//     }
+// }
+//  const milad=data.name;
+//  const adres=data.address.city;
+// const shahr=data.address.street;
+// const mmd =` سلام اقای  ${milad} شما سانکن ایرن شهر هتید  ${adres}و در این خیابان زندگی میکنید   ${shahr} `
+// console.log(mmd);
+// const ata=data.address.geo.name ;
+// alert ("welcome to your web ");
+//  var prommpt= prompt ("please say your name ");
+//  console.log(prommpt);
+//  if(prommpt>40){
+//     console.log("your age old")
+//  }else{
+//     console.log("your age is teen ")
+//  }
+// var age = prompt("سن خود را وارد کنید ");
+// if (age>50);
+// {"you are old man "}
+// else (age)
+// {alert ("سن شما این است " + age +"$")}
+// console.log(age);
+// alert("hi how you doing broher ");
+// console.log(alert);
+//  const age =prompt ('how old are you ');
+//  const round =Math.round(Number(age));
+//  console.log(age);
+// if(age>=50 ){
+//     alert ("you are very old ")
+
+// }
+// else
+// {
+//     alert ("you are dosent old ")
+// }
+// alert("thanks alot ");
+
+// alert (round); 
+
+// function name( num1 , num2 , num3) {
+//   let result= (num1+num2)*num3;  
+// prompt(result);
+// };
+// name(5,6,7);
+// function toCentigrad(faren) {
+//   const result = (5 / 9) * (faren - 32);
+// const temperature = toCentigrad(200);
+// console.log(temperature);
+// function name(name1) {
+//   prompt("its your name is a"  ,+ name1);
+// }
+// name(prompt("yourr name is "  +name));
+
+
+
+
+// const numbers = [2, 4, 6, 1,90,88,898];
+//  const filteredNumbers = numbers.filter(item <= item <=10);
+
+// console.log(filteredNumbers);
+
+
+
+
+
+
+
+
+
+// let num ={ age:120,name:'Ali',};
+
+// num.age=123;
+
+// num.name="ahmad";
+// console.log(num);
+
+//  const dda=prompt( "لطفا عدد خود را وارد کنید " );
+//  add=dda;
+// console.log(add);
+// if (dda % 2===0 ){
+
+
+//     alert(" add shoma zoj ast " ) 
+
+// }
+// else{
+
+// alert("add shoma fard ast    ")
+
+
+// };
+
+//  const add1= prompt("لطفا عدد خود را وارد کنید ");
+//  const add2 = prompt(" تا چه عددی باشد ");
+
+//  for(let i=add1 ; i<=add2 ; i++  ) {
+
+// if (i%2==0  ){
+//     alert(i)
+// }
+// if (i%2==1) {
+// alert(i)
+// }
+
+
+//  };
+
+// var add1 = prompt(" add aval ra vared kon ");
+//  var add2 = prompt(" add dovom  ra vared kon ");
+// var add = prompt(" alamat khod ra vared kon <+ - * />");
+// add1=Number(add1);
+// add2=Number(add2);
+// switch (add) {
+// case "+":
+// alert(add1+add2);    
+// break;
+// case "-":
+// alert(add1-add2);    
+//  break;
+//  case "*":
+//  alert(add1*add2);    
+//  break;
+// case "/":
+//  alert(add1/add2);    
+// break;}
+
+// let add = prompt("hاز چه عددی باشد  ");
+// let sum= 0 ;
+// for( let i=1; i<=add; i++  ){
+
+//     let add = prompt(""+i+"hاز چه عددی باشد  ");
+//     console.log(add+i);
+// add=Number(add);
+// sum+=add;
+
+    
+// };
+
+// const userData = [
+//     { name: "milad", age: 24 },
+//     { name: "milad", age: 24 },
+//     { name: "milad", age: 24 },
+//     { name: "milad", age: 24 },
+//     { name: "milad", age: 24 },
+//     { name: "Mamad", age: 22 },
+//     { name: "David", age: 20 },
+//     { name: "Thomas", age: 18 },
+//     { name: "Thomas", age: 18 },
+//     { name: "Thomas", age: 18 },
+//     { name: "Thomas", age: 18 },
+//     { name: "Thomas", age: 18 },
+//     { name: "Thomas", age: 18 },
+// ]
+// const ul = document.createElement("ul");
+// // console.log(ul);
+// userData.forof(item => {
+//     const li = document.createElement("li");
+//     li.innerText = `${item.name}'s age is ${item.age}`
+//     ul.appendChild(li)
+//     console.log(li)
+
+//  } )
+//  document.querySelector(".container").appendChild(ul)
+
+
+// console.log(ul);
+
+
+
+// const button = document.querySelector("button");
+// button.addEventListener("click", function() {
+//     alert(" اقا جان کلیک شد دیگه ");
+//     console.log("اقا جان کلیک شد دیگه ");
+    
+// } );
+
+// const h1= document.getElementById("title");
+
+// localStorage.setItem('name ' ,' atat ');
+// localStorage.setItem('name ' ,' atat65465 ');
+
+// localStorage.setItem('name3 ' ,' ata21 ');
+
+// localStorage.setItem('name 4' ,' atat365131 ');
+
+// // prompt(h1);
+// const name = localStorage.getItem('name ','name3 ','name 4');
+// console.log(name);
+// name ? h1.innerHTML=`welcome ${name}`: h1.innerText=" nobody to welcome";const h1 = document.getElementById("title");
+// const h1 = document.getElementById("title");
+// // ذخیره‌سازی اطلاعات در localStorage
+// localStorage.setItem('name', 'عطا ');
+// localStorage.setItem('name2', 'حسین ');
+// localStorage.setItem('name3', 'حسن ');
+// localStorage.setItem('name4', 'ممد ');
+
+// // کلیدهای مربوط به مقادیر
+// const keys = ['name', 'name2', 'name3', 'name4'];
+
+// // انتخاب رندوم یک کلید
+// const randomKey = keys[Math.floor(Math.random() * keys.length)];
+
+// // دریافت مقدار مرتبط با کلید رندوم
+// const name = localStorage.getItem(randomKey);
+
+// // نمایش پیام مناسب
+// name ? h1.innerHTML = `welcome ${name}` : h1.innerText = "nobody to welcome";
+// const ata =" ali jon2 " ;
+// localStorage.setItem("NAME "," ali agha ");
+// localStorage.getItem("name ");
+// localStorage.getItem( "name2",JSON.stringify(ata) );
+//  const add= localStorage.getItem( "name3",JSON.stringify(ata) );
+//  setInterval (time,100);
+// function time(){
+//   clearInterval(time,)
+//   console.log
+//   ("سلام به به کار کرد که ")
+// };
+
+
+// console.log("شروع برنامه...");
+
+// setInterval(() => {
+//     let currentTime = new Date();
+//     prompt("زمان فعلی:", currentTime.toLocaleTimeString());
+// }, 10000);
+// console.log("salam how are you ");
+// var add = 0; 
+// var timer = null; 
+// function loop() {
+//   add++;
+//   $().text(add);
+
+// }
+// function play () {
+//    timer= setInterval(loop,1000);
+//   }
+// function stop(){
+// if(timer != null );
+//   clearInterval(timer);
+// }
+// $(document).ready(function(){
+// });
+
+// var numbers= [10,2,66,99,54,21,20,15,24,87,69];
+//  var propmt =prompt("add khod ra vared konid ")
+
+//  var number;
+// for(number of numbers){
+// if(number === `${propmt}` ){
+//   console.log(`${number} find a number `);
+  
+// }
+// };
+// var person ={
+// fname :"ata",
+// lname:"hoseinnezhad",
+// age:17,
+// color: "sky"
+// };
+// var pkey;
+// for(pkey in person){
+//   console.log(`${pkey}:${person[prompt.person]} `);
+  
+// }
+
+
+// let arr = [1,2,3,4];
+// let result = arr.reduce(function(a,b,c){
+//   return a+b+c;
+// })
+// console.log(result);
+
+// function person() {
+//   this.fristname="ata ";
+//   this.lastname="hoseinnezhad";
+// }
+// let ata =new person ();
+// console.log(ata);
+
+// const hh2 = document.createElement('h2');
+// console.log(hh2);
+// // hh2.className = "اندازش بزرگه ";
+// // hh2.id = "ایدی";
+// hh2. innerHTML="اندازش بزرگه و دارای ایدی میباشد ";
+// // hh2.innerText="این چی هست حالا ";
+
+
+
+
+// const  arrayOfQuotes = [
+// {'author': 'حسین پناهی ', 'quote':'اگر وقت نداری حالم را بپرسی درک میکنم اگر وقت نداری من را ببینی درک میکنم اما اگر بعد از تمام اینها دیگر دوستت نداشتم اینبار نوبت توست درکم کنی '},
+// {'author': 'شبلی ', 'quote':'مرا تنها یک دل که مجذوب تو گردید نیست بلکه هر عضوم قلبی مشتاق است به سوی تو مایل '},
+// {'author': 'مولانا ', 'quote':'من زخم تو را به هیچ مرهم ندهم یک موی تو را به هر دو عالم ندهم '},
+// {'author': 'مهدی اخوان ', 'quote':'من اینجا پس دلم تنگ است و هر سازی که ببینم بد اهنگ است بیا ره توشه برداریم قدم در راه بی برگشت بگذاریم ببینیم اسمان هر کجا ایا همین رنگ است '},
+// {'author': 'وحشی بافقی ', 'quote':'عاشق شده ام بر وی بر. وی شده ام عاشق.یک سر دل من او برد . برد او دل من یک سر '},
+// {'author': 'شهریار ', 'quote':'به خود باز امدم نقش تو در خود جستجو کردم ..'},
+// {'author': 'سعدی', 'quote':'خوش تر از نقش تو در عالم تصویر نبود '},
+// {'author': 'امیر حسین حسن زاده ', 'quote':'موی فر داری شماو دلربایی میکنی . دلبر مو فرفری در دل خدایی میکنی'},
+// {'author': 'محمد علی بهمنی ', 'quote':'ار نگاه روی تو دل سیر میگردد مگر .اخر ادم با تو پیر میگردد مگر '},
+// {'author': 'مولانا ', 'quote':'گفته بودم اگر بوسه دهی . توبه کنم که دگر با تو از این خطاها نکنم . بوسه دادی و چو برخاست لبت از لب من . توبه کردم که دگر توبه ی بیجا نکنم '},
+// {'author': ' فریدون فرخزاد ', 'quote':'هر چه اید به سرمباز بگویم گذرد . وای ازین عصر که با میگذرد میگذرد ..'},
+// ];
+// function generateQuote(){
+//  const random = Number.parseInt(Math.random() * arrayOfQuotes.length + 1 );
+// document.querySelector('#quoteOutput').textContent =`"${arrayOfQuotes[random].quote}"`;
+//   document.querySelector('#authorOutput').textContent = `--${arrayOfQuotes[random].author}`;
+
+
+// }    
---
+++ node.js
@@ -0,0 +1,231 @@
+// const express = require('express');
+// const app = express();
+// app.get('/',function (req ,res) {
+// res.send('hello ata ')
+// });
+// app.listen(3000,()=>{
+// console.log('server is running on port 3000');
+// });
+
+
+// const fs = require('fs');
+// fs.readFile('mysite.txt', 'utf8', (err,data)=>{
+// if (err){
+//     console.log( 'err readingfile :'+err);
+//     return;
+// }
+// else  console.log('file content:' +data );
+// });
+// console.log('reading file..... (this runs frist)');
+
+
+// const http = require('http');
+// http.createServer((req,res )=>{
+// res.writeHead(200,{'Content-type': 'text/plain'});
+// res.end('heloo word!');
+// }).listen(3000);
+
+// let name = 'node.js';
+// const version= 20 ;
+// function greet(user) {
+//     return `helooo ,${user}!`;   
+// }
+// const add = (a,b )=>a+b;
+// console.log(greet('developer'));
+// console.log(add(5,3));
+// const user = {
+//     name : ' ata ',
+//     age :   17,
+//     greet (){
+//         console.log(`hi , im ${this.name}`);
+        
+//     }};
+
+// const colors = ['red', ' green ' , ' black'];
+// colors.forEach(color=>console.log(color));
+// const lengths = colors.map(color=>color.length);
+
+
+// function fetchData(callback) {
+//   setTimeout(() => {
+//     callback('Data received!');
+//   }, 1000);
+// }
+
+// // 2. Promises (ES6+)
+// const fetchDataPromise = () => {
+//   return new Promise((resolve) => {
+//     setTimeout(() => resolve('Promise resolved!'), 1000);
+//   });
+// };
+
+// // 3. Async/Await (ES8+)
+// async function getData() {
+//   const result = await fetchDataPromise();
+//   console.log(result);
+// }
+
+// getData(); // Call the async function
+// console.log(`V8 version: ${process.versions.v8}`);
+
+
+
+
+// const fs = require('fs');
+// console.log('start of bloocking code ');
+// const data = fs.readFileSync('myfile.txt',  'utf8');
+// console.log(' bloocking operation completed');
+
+
+// console.log( 'start of non bloocking code ');
+// fs.readFile('myfile.txt', 'utf8', (err,data )=>{
+// if (err) throw err;
+// console.log('non bloocking operation complated ');
+// });
+// console.log(' this runs before the file is read ');
+
+
+// const fs = require('fs');
+// console.log('1. Starting sync read...');
+// const data = fs.readFileSync('myfile.txt', 'utf8');
+// console.log('2. File contents:', data);
+// console.log('3. Done reading file');
+
+
+
+//  const fs = require('fs');
+// console.log('1. Starting async read...');
+// fs.readFile('myfile.txt', 'utf8', (err, data) => {
+//   if (err) throw err;
+//   console.log('2. File contents:', data);
+// });
+// console.log('3. Done starting read operation');
+
+
+
+// const express = require('express');
+// const config = require('./config');
+// const app = express();
+// global.config = require('./config');
+
+// app.use(express.static(__dirname + 'java')); // یا هر پوشه‌ای که داری
+
+// // مسیر اصلی
+// app.get('/', (req, res) => {
+//   res.send('Welcome to my server!');
+// });
+
+// // مسیر با پارامتر username
+// app.get('/:username', (req, res) => {
+//   console.log(req.params);
+//   res.send(`Hello ${req.params.username}`);
+// });
+
+// app.listen(config.port, () => {
+//   console.log(`server is running on port ${config.port}`);
+// });
+
+
+
+
+// const fs = require('fs').promises;
+// const promise1 = Promise.resolve('First result');
+// const promise2 = new Promise((resolve) => setTimeout(() => resolve('Second result'), 1000));
+// const promise3 = fs.readFile('myfile.txt', 'utf8'); // Read local file instead of fetch
+
+// Promise.all([promise1, promise2, promise3])
+//   .then(results => {
+//     console.log('Results:', results);
+//     // results[0] is from promise1
+//     // results[1] is from promise2
+//     // results[2] is the content of myfile.txt
+//   })
+//   .catch(error => {
+//     console.error('Error in one of the promises:', error);
+//   });
+
+
+// const myPromise= new Promise((resolve,reject)=>{
+// const succes = true ;
+// if (succes){
+//   resolve(' good luck ');
+// }
+// else{
+
+//   reject('dosent luck')
+// }
+
+
+// }); 
+//  myPromise
+// .then(result => console.log('Success:', result)) // وقتی موفق شد
+//  myPromise.catch(error => console.error('Error:', error)); 
+
+
+// async function getData() {
+//   console.log('Starting...');
+//   const result = await someAsyncOperation();
+//   console.log(`Result: ${result}`);
+//   return result;
+// }
+
+// function someAsyncOperation() {
+//   return new Promise(resolve => {
+//     setTimeout(() => resolve('Operation completed'), 1000);
+//   });
+// }
+
+// // Call the async function
+// getData().then(data => console.log('Final data:', data));
+
+
+
+
+
+
+
+// const fs = require('fs').promises;
+
+// async function loadUserData(userId) {
+//   try {
+//     const data = await fs.readFile(`users/${userId}.json`, 'utf8');
+//     const user = JSON.parse(data);
+
+//     if (!user.email) {
+//       throw new Error('Invalid user data: missing email');
+//     }
+
+//     return user;
+//   } catch (error) {
+//     // Handle different error types
+//     if (error.code === 'ENOENT') {
+//       throw new Error(`User ${userId} not found`);
+//     } else if (error instanceof SyntaxError) {
+//       throw new Error('Invalid user data format');
+//     }
+//     // Re-throw other errors
+//     throw error;
+//   } finally {
+//     // Cleanup code that runs whether successful or not
+//     console.log(`Finished processing user ${userId}`);
+//   }
+// }
+
+// // Usage
+// (async () => {
+//   try {
+//     const user = await loadUserData(123);
+//     console.log('User loaded:', user);
+//   } catch (error) {
+//     console.error('Failed to load user:', error.message);
+//     // Handle error (e.g., show to user, retry, etc.)
+//   }
+// })();
+
+
+
+
+
+
+
+// describe('async',function(){...});
